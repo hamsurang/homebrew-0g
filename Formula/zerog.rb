ee10cc721342dc9a06fa833f73236e5349c68c87
chaaerim <cofla411@ewhain.net>
2024-06-09 18:56:52 +0900

    feat: change version

--- Formula/zerog.rb
+++ Formula/zerog.rb
@@ -1,9 +1,9 @@
 class Zerog < Formula
     desc "A simple CLI-based todo application"
     homepage "https://github.com/hamsurang/homebrew-0g"
-    url "https://github.com/hamsurang/homebrew-0g/releases/download/v2.0.0/0g"
-    version "2.0.0"
-    sha256 "d33c29dbc649daa45bd4d4c9e63699b57adf279d8223e9c1465e7cba4da8d72c"
+    url "https://github.com/hamsurang/homebrew-0g/releases/download/v3.0.0/0g"
+    version "3.0.0"
+    sha256 "16f3f70bdfcecf30162ac9c8d3098a894e9d505261e57a247b15fb19e7e5d597"
   
     def install
       bin.install "0g"
