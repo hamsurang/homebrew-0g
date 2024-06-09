d90b3f6512ab096d808e37b77285a9990af19984
chaaerim <cofla411@ewhain.net>
2024-06-09 16:38:27 +0900

    fix: change url

--- Formula/secret.rb
+++ Formula/secret.rb
@@ -1,7 +1,7 @@
 class Secret < Formula
     desc "A simple CLI-based todo application"
     homepage "https://github.com/hamsurang/secret"
-    url "https://github.com/hamsurang/secret/releases/download/v1.0.0/test"
+    url "https://github.com/hamsurang/secret/releases/download/v1.0.0/secret-1.0.0.tar.gz"
     version "1.0.0"
     sha256 "SHA256_CHECKSUM"
   
